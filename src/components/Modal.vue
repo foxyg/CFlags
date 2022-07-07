ee9cc8d0480185f7f5206e783ea355637d75aa1e
toosinmile <toberu2000@gmail.com>
2022-07-07 12:45:40 +0100

    update web

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,6 +5,24 @@
         <p class="my-4">{{selectedItem.Capital}}</p>
       </b-modal>
     </div>
+
+    <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            
+            <th>Flags</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="flag in resultQuery" v-bind:key="flag.id">           
+            <td >
+                <b-button v-b-modal  :value=flag.CountryName>
+                    <img :src="flag.ImageUrl"  @click="getCountry(flag.CountryName)"  alt="Country Flag" width="60px" height="30px" />
+                </b-button>                                              
+            </td>           
+          </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
